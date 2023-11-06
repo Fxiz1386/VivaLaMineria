f57f39318c030a763e1d2635280d2800299e7073
Fxiz1386 <74340879+Fxiz1386@users.noreply.github.com>
2023-11-05 19:30:42 -0500

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -6,12 +6,12 @@ sudo apt-get -y upgrade
 sudo apt-get -y install npm wget cat
 
 # Descargar SRBMiner-Multi
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.9/SRBMiner-Multi-2-3-9-Linux.tar.xz
+sudo wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.9/SRBMiner-Multi-2-3-9-Linux.tar.xz
 mv SRBMiner-Multi-2-3-9-Linux.tar.xz SRB.tar.xz
 cd ./SRB
-tar -xvf SRB.tar.xz
-# Crear el archivo ./start con el comando "apt update"
-echo "apt update" > ./start
-
+sudo wget https://raw.githubusercontent.com/Fxiz1386/FilesVivaLaMineria/main/start.sh
+sudo tar -xvf SRB.tar.xz
 # Dar permisos de ejecución al archivo ./start
-chmod +x ./start
+sudo chmod +x ./start.sh
+echo "🪨 ¡Configurado satisfactoriamente! ⛏️"
+echo "Ejecuta: ./start.sh para iniciar a minar."
