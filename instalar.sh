9df1182b07f0785664be98fa13d3581a6e7aba34
Fxiz1386 <74340879+Fxiz1386@users.noreply.github.com>
2023-11-05 23:18:27 -0500

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -9,8 +9,6 @@ echo "(2/4) ⛏️🪨 Instalando..."
 wait 4
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install npm wget cat
-y
 wait 4
 echo "(3/4) ⛏️🪨 Descargando archivos..."
 sudo wget https://raw.githubusercontent.com/Fxiz1386/FilesVivaLaMineria/main/start.sh
@@ -23,9 +21,9 @@ mv SRBMiner-Multi-2-3-9/ SRB/
 mv ./start.sh SRB/
 rm ./SRBMiner-Multi-2-3-9-Linux.tar.xz
 rm -f ./instalar.sh
+sudo apt-get -y install npm
 npm i -g pm2
 cd SRB/
-clear
 echo "⚠️ Se ha creado el directorio > /SRB/"
 echo "(4/4) 🪨 ¡Configurado y instalado satisfactoriamente! ⛏️"
 echo "pm2 start ./start.sh para empezar a minar."
