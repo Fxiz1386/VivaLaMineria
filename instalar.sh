b23c8ae0474ea5a0a42bd069c0c021fa64628087
Fxiz1386 <74340879+Fxiz1386@users.noreply.github.com>
2023-11-06 20:44:25 -0500

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -2,30 +2,41 @@
 clear
 BLA_classic=( 0.25 '-' "\\" '|' '/' )
 echo "
-╔═══.·:·.☽✧ ⛏️ ✧☾.·:·.═══╗
-   /───SRBMiner-Linux────·
-  |─Instalador v{1.0}────·
-   \──© Fxiz1386 - 2023────·
-╚═══.·:·.☽✧ ⛏️ ✧☾.·:·.═══╝
+⣿⣿⣿⣿⣿⣟⠛⠛⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣷⣦⣀⠀⠀⠈⠙⢿⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⡀⠀⠸⣧⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣦⡀⠈⠛⠛⢿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⣹⣿⣦⡀⠀⠀⢻⣿⣷⠀⢠⣿⣿⡇⠀⠀⠀⣿
+⣿⣿⣿⣿⣿⣿⣿⠟⠋⠀⣠⣾⣿⣿⣿⣷⣄⠀⠈⣿⣿⠀⣾⣿⣿⣿⠀⠀⠀⣿
+⣿⣿⣿⣿⣿⠟⠁⠀⣠⣾⣿⣿⡟⠙⠻⣿⣿⣧⡀⢸⣿⣾⣿⣿⣿⣿⡆⠀⠀⣿
+⣿⣿⣿⡟⠁⠀⣠⣾⣿⣿⣿⣿⣶⣦⣤⣤⣭⣿⣷⣼⣿⣿⣿⠋⠉⠁⠀⠀⠀⣿
+⣿⡿⠋⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠋⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⣿
+⣿⣿⣦⣾⣿⣿⣿⣿⣿⣿⣿⡿⠟⠋⠁⠀⠀⢸⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠼⣿⣿⣿⣿⣿⣿⣶⣄⠀⠀⠀⣿
+⣿⣿⣿⣿⠛⠛⠻⠿⢿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠉⠛⢻⣿⣿⣿⠟⠀⠀⠀⣿
+⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡏⠀⠀⠀⠀⣿
+⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠛⠂⠀⠀⠀⣿
+⣿⣿⣿⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣿
 "
-echo "Presiona una tecla para iniciar la instalación"
+echo "SRBMiner-Linux install script"
+echo "Presiona una tecla para iniciar la instalación."
 read -n 1 -s -r -p ""
 BLA_metro=( 0.2 '[    ]' '[=   ]' '[==  ]' '[=== ]' '[ ===]' '[  ==]' '[   =]' )
-wait 4
+wait 7
 BLA_classic=( 0.25 '-' "\\" '|' '/' )
 echo "(2/4) ⛏️ Instalando..."
-wait 4
+wait 7
 BLA_classic=( 0.25 '-' "\\" 'q|' '/' )
 sudo apt-get -y update
 sudo apt-get -y upgrade
-wait 4
+wait 7
 BLA_classic=( 0.25 '-' "\\" '|' '/' )
 echo "(3/4) ⛏️ Descargando archivos..."
 sudo wget https://raw.githubusercontent.com/Fxiz1386/VivaLaMineria/main/start.sh
 sudo chmod +x ./start.sh
-wait 4
+wait 7
 sudo wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.9/SRBMiner-Multi-2-3-9-Linux.tar.xz
-wait 4
+wait 7
 sudo tar -xvf ./SRBMiner-Multi-2-3-9-Linux.tar.xz
 mv SRBMiner-Multi-2-3-9/ SRB/
 mv ./start.sh SRB/
