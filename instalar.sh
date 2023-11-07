43d0552622a6175aced6b0c1037492594964c171
Fxiz1386 <74340879+Fxiz1386@users.noreply.github.com>
2023-11-06 20:33:01 -0500

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -8,20 +8,20 @@ echo "
    \──© Fxiz1386 - 2023────·
 ╚═══.·:·.☽✧ ⛏️ ✧☾.·:·.═══╝
 "
-echo "presiona una tecla para iniciar la instalación"
+echo "Presiona una tecla para iniciar la instalación"
 read -n 1 -s -r -p ""
 BLA_metro=( 0.2 '[    ]' '[=   ]' '[==  ]' '[=== ]' '[ ===]' '[  ==]' '[   =]' )
 wait 4
 BLA_classic=( 0.25 '-' "\\" '|' '/' )
 echo "(2/4) ⛏️ Instalando..."
 wait 4
-BLA_classic=( 0.25 '-' "\\" '|' '/' )
+BLA_classic=( 0.25 '-' "\\" 'q|' '/' )
 sudo apt-get -y update
 sudo apt-get -y upgrade
 wait 4
 BLA_classic=( 0.25 '-' "\\" '|' '/' )
 echo "(3/4) ⛏️ Descargando archivos..."
-sudo wget https://raw.githubusercontent.com/Fxiz1386/FilesVivaLaMineria/main/start.sh
+sudo wget https://raw.githubusercontent.com/Fxiz1386/VivaLaMineria/main/start.sh
 sudo chmod +x ./start.sh
 wait 4
 sudo wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.9/SRBMiner-Multi-2-3-9-Linux.tar.xz
@@ -33,7 +33,7 @@ rm ./SRBMiner-Multi-2-3-9-Linux.tar.xz
 rm -f ./instalar.sh
 sudo apt-get -y install npm
 npm i -g pm2
-cd SRB/
+/SRB/
 clear
 echo "Se ha creado el directorio SRB/"
 echo "(4/4) ⛏️ ¡Configurado y instalado correctamente!"
