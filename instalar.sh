aac2c3e38b58f9a053e68196e774c3948e06c9fe
Fxiz1386 <74340879+Fxiz1386@users.noreply.github.com>
2023-11-08 15:08:57 -0500

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 clear
-BLA_classic=( 0.25 '-' "\\" '|' '/' )
 echo "
 ⣿⣿⣿⣿⣿⣟⠛⠛⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
 ⣿⣿⣿⣿⣿⣿⣷⣦⣀⠀⠀⠈⠙⢿⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⣿
@@ -18,20 +17,16 @@ echo "
 ⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠛⠂⠀⠀⠀⣿
 ⣿⣿⣿⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣿
 "
-echo "SRBMiner-Linux install script"
-echo "Presiona una tecla para iniciar la instalación."
+echo "      SRBMiner-Linux install script"
+echo "      Presiona una tecla para iniciar la instalación."
 read -n 1 -s -r -p ""
-BLA_metro=( 0.2 '[    ]' '[=   ]' '[==  ]' '[=== ]' '[ ===]' '[  ==]' '[   =]' )
 wait 7
-BLA_classic=( 0.25 '-' "\\" '|' '/' )
-echo "(2/4) ⛏️ Instalando..."
+echo ".     (2/4) ⛏️ Instalando..."
 wait 7
-BLA_classic=( 0.25 '-' "\\" 'q|' '/' )
 sudo apt-get -y update
 sudo apt-get -y upgrade
 wait 7
-BLA_classic=( 0.25 '-' "\\" '|' '/' )
-echo "(3/4) ⛏️ Descargando archivos..."
+echo "      (3/4) ⛏️ Descargando archivos..."
 sudo wget https://raw.githubusercontent.com/Fxiz1386/VivaLaMineria/main/start.sh
 sudo chmod +x ./start.sh
 wait 7
@@ -44,8 +39,6 @@ rm ./SRBMiner-Multi-2-3-9-Linux.tar.xz
 rm -f ./instalar.sh
 sudo apt-get -y install npm
 npm i -g pm2
-' >> /SRB/
-echo "
-      Se ha creado el directorio SRB/"
-echo "(4/4) ⛏️ ¡Configurado y instalado correctamente!"
-echo "pm2 start ./start.sh para empezar a minar."
+echo "     Se ha creado el directorio SRB/"
+echo "     (4/4) ⛏️ ¡Configurado y instalado correctamente!"
+echo "     pm2 start ./start.sh para empezar a minar."
