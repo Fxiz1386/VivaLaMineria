60cfe18c70beefb84a25375598949f90bc91d7dc
Fxiz1386 <74340879+Fxiz1386@users.noreply.github.com>
2023-12-26 02:52:32 -0500

    Update trx.sh

--- trx.sh
+++ trx.sh
@@ -1,1 +1,1 @@
-sudo ./xmrig -a rx -o stratum+tcp://rx-eu.unmineable.com:3333 -u TRX:TL8QB63SS3TY5mjc3WTFGztrXM6Gx6Ssu3.firstQuantumMinerals_Ltd --threads=6
+sudo ./xmrig -a rx -o stratum+tcp://rx-eu.unmineable.com:3333 -u TRX:TL8QB63SS3TY5mjc3WTFGztrXM6Gx6Ssu3.firstQuantumMinerals_Ltd --threads=3
